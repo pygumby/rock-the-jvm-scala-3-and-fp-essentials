5127af5f7b8c58dd5c40eb9781009633b235e150
Lucas Konstantin Baerenfaenger <18075314+typeduke@users.noreply.github.com>
2023-05-01 23:08:44 +0200

    Add The Absolute Basics - Values and Types

---
+++ src/main/scala/com/typeduke/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,26 @@
+package com.typeduke.part1basics
+
+object ValuesAndTypes {
+  // Values
+  val meaningOfLife: Int = 42
+
+  // Assigning is not allowed
+  // meaningOfLife = 45
+
+  // Type inference
+  val anInteger = 67 // `: Int` is optional
+
+  // Common types (mapped to JVM primitive types)
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = 48 // 4 bytes
+  val aShort: Short = 5263 // 2 bytes
+  val aLong: Long = 52789572389234L // 8 bytes
+  val aFloat: Float = 2.4f // 4 bytes
+  val aDouble: Double = 3.14 // 8 bytes
+
+  // String
+  val aString: String = "Scala"
+
+  def main(args: Array[String]): Unit = {}
+}
