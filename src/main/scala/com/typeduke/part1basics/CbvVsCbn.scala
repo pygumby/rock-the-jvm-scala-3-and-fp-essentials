cfe943452fd584eb74e0774c6421f9f907fa2be3
Lucas Konstantin Baerenfaenger <18075314+typeduke@users.noreply.github.com>
2023-05-08 18:49:42 +0200

    Add The Absolute Basics - Call by Name and Call by Value

---
+++ src/main/scala/com/typeduke/part1basics/CbvVsCbn.scala
@@ -0,0 +1,36 @@
+package com.typeduke.part1basics
+
+object CbvVsCbn {
+  // CBV = call by value = arguments are evaluated before function invocation.
+  def aFunction(arg: Int): Int = arg + 1
+  val aComputation = aFunction(23 + 67)
+
+  // CBN = call by name = arguments are passed LITERALLY, evaluated at every reference.
+  def aByNameFunction(arg: => Int): Int = arg + 1
+  val anotherComputation = aByNameFunction(23 + 67)
+
+  def printTwiceByValue(x: Long): Unit = {
+    println(f"By value: $x")
+    println(f"By value: $x")
+  }
+
+  // Argument evaluation is delayed.
+  // Argument is evaluated every time it is used.
+  def printTwiceByName(x: => Long): Unit = {
+    println(f"By name: $x")
+    println(f"By name: $x")
+  }
+
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+
+  def main(args: Array[String]): Unit = {
+    println(aComputation)
+    println(anotherComputation)
+
+    printTwiceByValue(System.nanoTime())
+    printTwiceByName(System.nanoTime())
+
+    printFirst(42, infinite())
+  }
+}
