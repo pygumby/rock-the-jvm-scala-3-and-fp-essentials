805866e1ecd50508be3ee8021d4d2dd83aff2dd3
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2023-09-04 21:03:15 +0000

    Add Object-Oriented Programming in Scala - Exercise: Extending MyList

--- src/main/scala/com/typeduke/practice/LList.scala
+++ src/main/scala/com/typeduke/practice/LList.scala
@@ -2,38 +2,9 @@ package com.typeduke.practice
 
 import scala.annotation.tailrec
 
-// Singly linked list
-abstract class LList[A] {
-  def head: A
-  def tail: LList[A]
-  def isEmpty: Boolean
-  def add(element: A): LList[A] = new Cons(element, this)
-}
-
-class Empty[A] extends LList[A] {
-  override def head: A = throw new NoSuchElementException
-  override def tail: LList[A] = throw new NoSuchElementException
-  override def isEmpty: Boolean = true
-
-  override def toString: String = "[]"
-}
-
-class Cons[A](override val head: A, override val tail: LList[A]) extends LList[A] {
-  override def isEmpty: Boolean = false
-
-  override def toString(): String = {
-    @tailrec
-    def concatenateElements(remainder: LList[A], acc: String): String =
-      if (remainder.isEmpty) acc
-      else concatenateElements(remainder.tail, s"$acc, ${remainder.head}")
-
-    s"[${concatenateElements(this.tail, s"$head")}]"
-  }
-}
-
 /* Exercise 1
  * LList extension
- * 
+ *
  * 1. Generic trait `Predicate[T]` with a little method `test(T) => Boolean`
  * 2. Generic trait `Transformer[A, B]` with a method `transform(A) => B`
  * 3. LList:
@@ -49,9 +20,88 @@ class Cons[A](override val head: A, override val tail: LList[A]) extends LList[A
  * `[1, 2, 3].flatMap(n => [n, n + 1]) => (1, 2, 2, 3, 3, 4)`
  */
 
+// Singly linked list
+abstract class LList[A] {
+  def head: A
+  def tail: LList[A]
+  def isEmpty: Boolean
+  def add(element: A): LList[A] = new Cons(element, this)
+
+  infix def ++(other: LList[A]): LList[A]
+
+  def map[B](transformer: Transformer[A, B]): LList[B]
+  def filter(predicate: Predicate[A]): LList[A]
+  def flatMap[B](transformer: Transformer[A, LList[B]]): LList[B]
+}
+
+class Empty[A] extends LList[A] {
+  override def head: A = throw new NoSuchElementException
+  override def tail: LList[A] = throw new NoSuchElementException
+  override def isEmpty: Boolean = true
+
+  override def ++(other: LList[A]): LList[A] = other
+
+  override def map[B](transformer: Transformer[A, B]): LList[B] = new Empty[B]
+  override def filter(predicate: Predicate[A]): LList[A] = this
+  override def flatMap[B](transformer: Transformer[A, LList[B]]): LList[B] = new Empty[B]
+
+  override def toString: String = "[]"
+}
+
+class Cons[A](override val head: A, override val tail: LList[A]) extends LList[A] {
+  override def isEmpty: Boolean = false
+
+  override def ++(other: LList[A]): LList[A] = 
+    new Cons(this.head, this.tail ++ other)
+
+  override def map[B](transformer: Transformer[A, B]): LList[B] =
+    new Cons(transformer.transform(this.head), this.tail.map(transformer))
+
+  override def filter(predicate: Predicate[A]): LList[A] =
+    if (predicate.test(this.head)) new Cons(this.head, this.tail.filter(predicate))
+    else this.tail.filter(predicate)
+
+  override def flatMap[B](transformer: Transformer[A, LList[B]]): LList[B] =
+    transformer.transform(this.head) ++ this.tail.flatMap(transformer)
+
+  override def toString(): String = {
+    @tailrec
+    def concatenateElements(remainder: LList[A], acc: String): String =
+      if (remainder.isEmpty) acc
+      else concatenateElements(remainder.tail, s"$acc, ${remainder.head}")
+
+    s"[${concatenateElements(this.tail, s"${this.head}")}]"
+  }
+}
+
+trait Predicate[T] {
+  def test(element: T): Boolean
+}
+
+class EvenPredicate extends Predicate[Int] {
+  override def test(element: Int): Boolean = element % 2 == 0
+}
+
+trait Transformer[A, B] {
+  def transform(element: A): B
+}
+
+class Doubler extends Transformer[Int, Int] {
+  override def transform(element: Int): Int = element * 2
+}
+
+class DoublerList extends Transformer[Int, LList[Int]] {
+  override def transform(element: Int): LList[Int] =
+    new Cons(element, new Cons(element * 2, new Empty))
+}
+
+class StringToIntTransformer extends Transformer[String, Int] {
+  override def transform(element: String): Int = element.toInt
+}
+
 object LListTest {
   def main(args: Array[String]): Unit = {
-    val empty = new Empty[Int]()
+    val empty = new Empty[Int]
 
     println(empty)
     println(empty.isEmpty)
@@ -66,5 +116,23 @@ object LListTest {
     val someStrings = new Cons("dog", new Cons("cat", new Empty))
 
     println(someStrings)
+
+    // `map` tests
+    val numbersDoubled = firstThreeNumbers.map(new Doubler)
+    println(numbersDoubled)
+    val numbersNested = firstThreeNumbers.map(new DoublerList)
+    println(numbersNested)
+
+    // `filter` tests
+    val onlyEvenNumbers = firstThreeNumbers.filter(new EvenPredicate)
+    println(onlyEvenNumbers)
+
+    // `concatenation` tests
+    val listInBothWays = firstThreeNumbers ++ firstThreeNumbers2
+    println(listInBothWays)
+
+    // `flatMap` tests
+    val flattenedList = firstThreeNumbers.flatMap(new DoublerList)
+    println(flattenedList)
   }
 }
