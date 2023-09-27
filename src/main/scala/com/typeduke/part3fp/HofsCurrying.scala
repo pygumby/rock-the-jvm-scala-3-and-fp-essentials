1e9c3eb39113bf8ccc229ec4d66a83dab3eee80e
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2023-09-27 18:04:53 +0000

    Add Functional Programming in Scala - HOFs & Currying: Exercises (part 2)

--- src/main/scala/com/typeduke/part3fp/HofsCurrying.scala
+++ src/main/scala/com/typeduke/part3fp/HofsCurrying.scala
@@ -39,11 +39,56 @@ object HofsCurrying {
   val standardFormat: (Double => String) = curriedFormatter("%4.2f") // x => "%4.2f".format(x)
   val preciseFormat: (Double => String) = curriedFormatter("%10.8f") // x => "%10.8f".format(x)
 
+  /* Exercise 1
+   * Create functions that apply and un-apply currying to a given function.
+   *
+   * toCurry(f: (Int, Int) => Int): Int => Int => Int
+   * fromCurry(f: Int => Int => Int): (Int, Int) => Int
+   */
+
+  def toCurry[A, B, C](f: (A, B) => C): A => B => C =
+    x => y => f(x, y)
+
+  val superAdder2 = toCurry[Int, Int, Int](_ + _) // equivalent to `superAdder`
+
+  def fromCurry[A, B, C](f: A => B => C): (A, B) => C =
+    (x, y) => f(x)(y)
+
+  val simpleAdder = fromCurry(superAdder2)
+
+  /* Exercise 2
+   * Create a functions that compose given functions.
+   *
+   * compose(f, g): x => f(g(x))
+   * andThen(f, g): x => g(f(x))
+   */
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    x => f(g(x))
+
+  val incrementer = (x: Int) => x + 1
+  val doubler = (x: Int) => 2 * x
+
+  val composedApplication = compose(incrementer, doubler)
+
+  def andThen[A, B, C](f: A => B, g: B => C): A => C =
+    x => g(f(x))
+
+  val sequencedApplication = andThen(incrementer, doubler)
+
   def main(args: Array[String]): Unit = {
     println(tenThousand)
     println(oneHundred)
 
     println(standardFormat(Math.PI))
     println(preciseFormat(Math.PI))
+
+    // Exercise 1
+    println(superAdder2(40)(2)) // 42
+    println(simpleAdder(40, 2)) // 42
+
+    // Exercise 2
+    println(composedApplication(42)) // 85
+    println(sequencedApplication(42)) // 86
   }
 }
