1ae31767db9b6c7afe650bc05ea81f168e59097f
Lucas Konstantin Bärenfänger <18075314+typeduke@users.noreply.github.com>
2023-10-14 11:37:23 +0000

    Run Scalafmt

--- src/main/scala/com/typeduke/part4power/PatternMatching.scala
+++ src/main/scala/com/typeduke/part4power/PatternMatching.scala
@@ -6,7 +6,7 @@ object PatternMatching {
   // Pattern matching = `switch` on steroids
   val random = new Random()
   val aValue = random.nextInt(100)
-  
+
   val description = aValue match {
     case 1 => "The first"
     case 2 => "The second"
@@ -20,7 +20,7 @@ object PatternMatching {
 
   val greeting = bob match {
     case Person(n, a) if a < 18 => s"Hi there, my name is $n and I'm $a years old." // `if` guard
-    case Person(n, _) => s"Hi there, my name is $n and I won't say my age."
+    case Person(n, _)           => s"Hi there, my name is $n and I won't say my age."
     // case _ => "I don't know who I am." // Unreachable, hence commented out
   }
 
@@ -50,12 +50,12 @@ object PatternMatching {
   case class Prod(e1: Expr, e2: Expr) extends Expr
 
   def show(e: Expr): String = e match {
-    case Number(n) => s"$n"
+    case Number(n)   => s"$n"
     case Sum(e1, e2) => show(e1) + " + " + show(e2)
     case Prod(e1, e2) => {
       def maybeShowParens(expr: Expr): String = expr match {
         case Sum(_, _) => s"(${show(expr)})"
-        case _ => show(expr)
+        case _         => show(expr)
       }
       maybeShowParens(e1) + " * " + maybeShowParens(e2)
     }
